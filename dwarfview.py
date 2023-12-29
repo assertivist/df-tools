47ed7e1089831f98d922508bb20f0ae8aa7f46f0
assertivist <andy.halstead@gmail.com>
2023-12-28 23:56:55 -0800

    scrolling no panning

--- dwarfview.py
+++ dwarfview.py
@@ -17,7 +17,7 @@ def arg_handle():
 def to_c_int16_arr(ls):
     return (ctypes.c_int16 * len(ls))(*ls)
 
-zoom = 60
+zoom = 5
 
 colors = {
     "Desert": [250, 206, 57, 255],
@@ -28,26 +28,40 @@ colors = {
     "Wetland": [96, 160, 132, 255],
     "Mountains": [189, 185, 185, 255],
     "Tundra": [210, 232, 237, 255],
-    "Grassland": [186, 237, 78, 255]
+    "Grassland": [186, 237, 78, 255],
+    "Glacier": [240, 255, 255, 255]
 }
 
-class LegendsView(pyui.View):
+class RegionMap(pyui.View):
     def __init__(self, data):
         super().__init__()
         self.data = data
+        self._surface = None
 
-    def draw(self, renderer, rect):
-        #boxRGBA(renderer, 10, 10, 20, 20, 0, 0, 0, 255)
+        self.rects = []
         for rid, name, rtype, coords, evilness in data:
             coordlist = coords.split("|")
             #print(coordlist)
             for coord in coordlist:
                 if len(coord) < 1:
                     continue
-                x = int(coord.split(",")[0]) * zoom
-                y = int(coord.split(",")[1]) * zoom
+                x = int(coord.split(",")[0])
+                y = int(coord.split(",")[1])
                 c = colors[rtype] if rtype in colors else [0, 0, 0, 255]
-                boxRGBA(renderer, x, y, x + zoom, y + zoom, *c)
+                self.rects.append((x, y, c))
+
+    def zoom(self, val):
+        self.zoom = val
+        return self
+
+    def draw(self, renderer, rect):
+        #boxRGBA(renderer, 10, 10, 20, 20, 0, 0, 0, 255)
+        for x,y,c in self.rects:
+            sx = self.zoom.value * x
+            sy = self.zoom.value * y
+            ex = sx + self.zoom.value
+            ey = sy + self.zoom.value
+            boxRGBA(renderer, sx, sy, ex, ey, *c)
             #vx = to_c_int16_arr([int(x.split(",")[0]) * zoom for x in coordlist])
             #vy = to_c_int16_arr([int(x.split(",")[1]) * zoom for x in coordlist])
             #n = len(coordlist)
@@ -55,6 +69,31 @@ class LegendsView(pyui.View):
             #print(vy)
             #polygonColor(renderer, vx, vy, n, 23423223)
 
+
+class LegendsMapView(pyui.View):
+    zoom = pyui.State(int, default=20)
+
+
+    def __init__(self, data):
+        super().__init__()
+        self.data = data
+
+    def zoom_in(self):
+        self.zoom += 5
+
+    def zoom_out(self):
+        self.zoom -= 5
+
+    def mousewheel(self, amt):
+        print(amt)
+        self.zoom.value += amt[1]
+
+
+    def content(self):
+        yield pyui.VStack(
+            pyui.Slider(self.zoom, 1, 100),
+            RegionMap(self.data).zoom(self.zoom),
+        )
 
 
 
@@ -66,9 +105,9 @@ if __name__ == "__main__":
     data = []
     it = cur.execute("select id,name,type,coords,evilness from regions")
     for row in it.fetchall():
-        print(row)
+        #print(row)
         data.append(row)
 
     app = pyui.Application("club.thingstead.DwarfLegendsDatabase")
-    app.window("DwarfLegends", LegendsView(data))
+    app.window("DwarfLegends", LegendsMapView(data))
     app.run()
