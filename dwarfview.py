7be8f1e6ff9aa8fe3c2f0183b84ecdd74f42d9a3
assertivist <andy.halstead@gmail.com>
2023-12-28 22:47:52 -0800

    basic dwarf view

--- dwarfview.py
+++ dwarfview.py
@@ -17,7 +17,19 @@ def arg_handle():
 def to_c_int16_arr(ls):
     return (ctypes.c_int16 * len(ls))(*ls)
 
-zoom = 1
+zoom = 60
+
+colors = {
+    "Desert": [250, 206, 57, 255],
+    "Forest": [41, 141, 17, 255],
+    "Hills": [136, 207, 69, 255],
+    "Lake": [40, 173, 207, 255],
+    "Ocean": [37, 67, 147, 255],
+    "Wetland": [96, 160, 132, 255],
+    "Mountains": [189, 185, 185, 255],
+    "Tundra": [210, 232, 237, 255],
+    "Grassland": [186, 237, 78, 255]
+}
 
 class LegendsView(pyui.View):
     def __init__(self, data):
@@ -28,14 +40,20 @@ class LegendsView(pyui.View):
         #boxRGBA(renderer, 10, 10, 20, 20, 0, 0, 0, 255)
         for rid, name, rtype, coords, evilness in data:
             coordlist = coords.split("|")
-            print(coordlist)
-
-            vx = to_c_int16_arr([int(x.split(",")[0]) * zoom for x in coordlist])
-            vy = to_c_int16_arr([int(x.split(",")[1]) * zoom for x in coordlist])
-            n = len(coordlist)
-            print(vx)
-            print(vy)
-            polygonColor(renderer, vx, vy, n, 23423223)
+            #print(coordlist)
+            for coord in coordlist:
+                if len(coord) < 1:
+                    continue
+                x = int(coord.split(",")[0]) * zoom
+                y = int(coord.split(",")[1]) * zoom
+                c = colors[rtype] if rtype in colors else [0, 0, 0, 255]
+                boxRGBA(renderer, x, y, x + zoom, y + zoom, *c)
+            #vx = to_c_int16_arr([int(x.split(",")[0]) * zoom for x in coordlist])
+            #vy = to_c_int16_arr([int(x.split(",")[1]) * zoom for x in coordlist])
+            #n = len(coordlist)
+            #print(vx)
+            #print(vy)
+            #polygonColor(renderer, vx, vy, n, 23423223)
 
 
 
