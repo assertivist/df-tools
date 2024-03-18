a42bc08fbe284a8310813724be8be62a28736d8d
assertivist <andy.halstead@gmail.com>
2024-03-17 22:41:51 -0700

    redis experiment that mostly works

---
+++ lua/dfdb.lua
@@ -0,0 +1,16 @@
+local dfdb = {
+    _VERSION = 'dfdb 0.0.1-dev',
+    _DESCRIPTION = 'A connection from DFHack to Redis',
+    _COPYRIGHT = 'Zlib'
+}
+
+local YIELD_TIMEOUT_MS = 10
+
+local redis = require('redis')
+local client = redis.connect({host="127.0.0.1",port=6379,scheme="tcp"})
+
+dfdb.client = client
+
+
+
+return dfdb
