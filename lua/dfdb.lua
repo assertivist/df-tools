f8a456666b3cc9a60c0161669c8549e4d283a839
assertivist <andy.halstead@gmail.com>
2024-03-17 22:46:02 -0700

    add a couple lines of example that worked

--- lua/dfdb.lua
+++ lua/dfdb.lua
@@ -9,6 +9,10 @@ local YIELD_TIMEOUT_MS = 10
 local redis = require('redis')
 local client = redis.connect({host="127.0.0.1",port=6379,scheme="tcp"})
 
+--client.set('foo', 'bar')
+--local value = c:get('foo')
+--dfhack.println(value)
+
 dfdb.client = client
 
 
